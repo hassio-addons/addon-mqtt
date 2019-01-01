82e6eaa7f1d895909db64c98bf1531f1f2870fa8
ludeeus <joasoe@gmail.com>
2019-01-01 20:37:23 +0100

    Add LUA buypass to non-ssl as well

--- mqtt/rootfs/etc/cont-init.d/40-nginx.sh
+++ mqtt/rootfs/etc/cont-init.d/40-nginx.sh
@@ -10,19 +10,16 @@ declare certfile
 declare keyfile
 
 # Only run this if the web part of the add-on are enabled.
-
-if hass.config.true 'leave_front_door_open'; then
-    # Remove LUA auth if leave_front_door_open == True
-    sed -i "/access_by_lua_file/d" /etc/nginx/nginx-ssl.conf
-    sed -i "/access_by_lua_file/d" /etc/nginx/nginx-ssl.conf
-fi
-
 if hass.config.true 'web.enabled'; then
+    # Remove LUA auth if leave_front_door_open == True
+    if hass.config.true 'leave_front_door_open'; then
+        sed -i "/access_by_lua_file/d" /etc/nginx/nginx.conf
+        sed -i "/access_by_lua_file/d" /etc/nginx/nginx-ssl.conf
+    fi
     # Enable SSL
     if hass.config.true 'web.ssl'; then
         certfile=$(hass.config.get 'certfile')
         keyfile=$(hass.config.get 'keyfile')
-
         sed -i "s/%%certfile%%/${certfile}/g" /etc/nginx/nginx-ssl.conf
         sed -i "s/%%keyfile%%/${keyfile}/g" /etc/nginx/nginx-ssl.conf
     fi
